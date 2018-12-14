4c3fd0acd0a26129ee3b2ad1894c0483d5203ca1
Salavija <huh.freak@gmail.com>
2018-12-14 19:25:59 +0200

    Some additions to other files. App works, however needs some fixing and upgrade

--- practical-exam2018/src/main/java/it/akademija/exam/controllers/LoginController.java
+++ practical-exam2018/src/main/java/it/akademija/exam/controllers/LoginController.java
@@ -1,4 +1,15 @@
 package it.akademija.exam.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.security.Principal;
+
 public class LoginController {
+    @GetMapping("/login")
+    public String login(Principal principal) {
+        if (principal != null) {
+            return "redirect:/home";
+        }
+        return "/login";
+    }
 }
