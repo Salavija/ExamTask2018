749af97061ec186dd5cec3494ab882028458c93a
Salavija <huh.freak@gmail.com>
2018-12-20 12:23:47 +0200

    Last React commit

--- beeps-react/src/AppContainer.js
+++ beeps-react/src/AppContainer.js
@@ -5,12 +5,12 @@ const AppContainer = (props) => {
     return (<div>
         <div>
             <Link to='/'>Home</Link> |&nbsp;
-            <Link to='/countries'>Products</Link> |&nbsp;
-            <Link to={`/countries/${127}`}>Product by no</Link> |&nbsp;
+            <Link to='/countries'>countries</Link> |&nbsp;
+            <Link to='/holidays'>holidays</Link> |&nbsp;
+            <Link to='/non-existant'>Non Existant</Link>
         </div>
         {props.children}
     </div>);
 };
 
 export default AppContainer;
-
